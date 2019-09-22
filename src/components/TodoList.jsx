6da0e0c34d9d7c1f8f40c486de3c651b48ec2a61
Fladrian <adriandev.fl@gmail.com>
2019-09-22 04:02:20 -0400

    Replace some components name, making it more readable and explicit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import ListTodos from './ListTodos'
+import Todos from './Todos'
 import FormTodos from './FormTodos'
 
 function TodoList({todos, handleSubmit, handleChange, refForm}) {
 	return (
 		<div className="container">
 			<div className="list-todos">
-				{ todos.map((todo) => <ListTodos key={todo.id} todo={todo} />) }
+				{ todos.map((todo) => <Todos key={todo.id} todo={todo} />) }
 			</div>
 			<div className="form-todos">
 				<FormTodos
