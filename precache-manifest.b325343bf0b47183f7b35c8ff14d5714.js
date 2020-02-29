26e52caede736c144bab452f67befe99476cbf64
Fladrian <adriandev.fl@gmail.com>
2020-02-29 02:17:55 -0400

    Updates

---
+++ precache-manifest.b325343bf0b47183f7b35c8ff14d5714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77bfd1ff215755dc36b071ab0f83dee2",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "a597e5d7fad1c4ee7401",
+    "url": "/react-todolist/static/css/main.6492aace.chunk.css"
+  },
+  {
+    "revision": "08f0d918af91621a6cb6",
+    "url": "/react-todolist/static/js/2.3188c175.chunk.js"
+  },
+  {
+    "revision": "a597e5d7fad1c4ee7401",
+    "url": "/react-todolist/static/js/main.5602c618.chunk.js"
+  },
+  {
+    "revision": "17dce66f815ad67ab565",
+    "url": "/react-todolist/static/js/runtime-main.6404541e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-todolist/static/media/logo.25bf045c.svg"
+  }
+]);
